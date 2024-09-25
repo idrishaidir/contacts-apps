f59cc3ad87cb21e639a3175cd1aafec2d2ab4a84
Idris Haidir Ali <aliidir006@gmail.com>
2024-09-25 20:35:27 +0700

    Membuat Contact list dan Contact Item

---
+++ src/components/ContactItemBody.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+function ContactItemBody({ name, tag }) {
+  return (
+    <div className="contact-item_body">
+      <h3 className="contact-item_title">{name}</h3>
+      <p className="contact-item_username">@{tag}</p>
+    </div>
+  );
+}
+
+export default ContactItemBody;
